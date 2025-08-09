3f508a9b201618bdf0ae0eaea9248f634ec27a46
Andres <parraandres723@gmail.com>
2025-08-09 12:53:29 -0400

    Normalizar saltos de línea

--- back/api/urls.py
+++ back/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import UsuarioViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UsuarioViewSet
+from .views import UsuarioViewSet, RegistroUsuarioView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,6 +13,7 @@ router.register(r'usuarios', UsuarioViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),       # Login
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),       # Login
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),      # Refrescar token
+    path('register/', RegistroUsuarioView.as_view(), name='registro'),          # Registro de usuario
 ]
--- back/api/views.py
+++ back/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from django.contrib.auth import get_user_model
 from .serializers import UsuarioSerializer
+from rest_framework import generics, permissions
+from .serializers import RegistroUsuarioSerializer
 
 Usuario = get_user_model()
 
@@ -9,3 +11,8 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
     permission_classes = [permissions.AllowAny]  # Cambia según tu seguridad
+
+
+class RegistroUsuarioView(generics.CreateAPIView):
+    serializer_class = RegistroUsuarioSerializer
+    permission_classes = [permissions.AllowAny]
